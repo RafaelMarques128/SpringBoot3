f4c61029dd420f9ec06ab1ce8f804d6c6be53732
Rafael Souza Marques <rsmarquesprog@gmail.com>
2024-08-21 10:22:33 -0300

    Repository e Service

---
+++ src/main/java/med/voll/api/domain/usuario/AutenticaoService.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Service
+public class AutenticaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
